fd8adcf803b0ff6ee1f328249588b3fdaa020f7b
Dhruvi Mathukiya <mathukiyag@gmail.com>
2026-03-11 12:57:29 +0530

    ServiceDetails

--- backend/controllers/helpControllers.js
+++ backend/controllers/helpControllers.js
@@ -1,21 +1,21 @@
-exports.helpController = async (req,res)=>{
+exports.helpController = async (req, res) => {
 
-try{
+    try {
 
-console.log("Help button clicked");
+        console.log("Help button clicked");
 
-res.status(200).json({
-success:true,
-message:"Redirecting to contact page"
-});
+        res.status(200).json({
+            success: true,
+            message: "Redirecting to contact page"
+        });
 
-}catch(err){
+    } catch (err) {
 
-res.status(500).json({
-success:false,
-message:"Server error"
-});
+        res.status(500).json({
+            success: false,
+            message: "Server error"
+        });
 
-}
+    }
 
 };
